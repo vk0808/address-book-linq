830be184a91f7d4f7c26b24b3e025b89bc031c26
vk0808 <vk4765712@gmail.com>
2021-09-11 16:13:11 +0530

    feat: extend dataTable

--- AddressBook/AddressDataTable.cs
+++ AddressBook/AddressDataTable.cs
@@ -22,13 +22,15 @@ namespace AddressBook
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+            table.Columns.Add("BookName", typeof(string));
+            table.Columns.Add("BookType", typeof(string));
 
             // uc-3
-            table.Rows.Add("Rahul", "Gowda", "2nd cross Ramnagar Nagar", "Mysore", "Karnataka", "580082", "8123351458", "rahul@gmail.com");
-            table.Rows.Add("Ravi", "Kumar", "3nd cross", "Bangalore", "Karnataka", "800078", "934747358", "ravi@gmail.com");
-            table.Rows.Add("Asha", "Kumari", "4nd cross Indira Nagar", "Bangalore", "Karnataka", "580003", "1234567896", "asha@gmail.com");
-            table.Rows.Add("Porvi", "Shetty", "2nd cross", "Mandya", "Karnataka", "560078", "0987654321", "porvi@gmail.com");
-            table.Rows.Add("Raja", "M", "2nd cross Rajaji Nagar", "Gadag", "Karnataka", "570006", "0987654321", "raja@gmail.com");
+            table.Rows.Add("Rahul", "Gowda", "2nd cross Ramnagar Nagar", "Mysore", "Karnataka", "580082", "8123351458", "rahul@gmail.com", "Book1", "Friends");
+            table.Rows.Add("Ravi", "Kumar", "3nd cross", "Bangalore", "Karnataka", "800078", "934747358", "ravi@gmail.com", "Book2", "Family");
+            table.Rows.Add("Asha", "Kumari", "4nd cross Indira Nagar", "Bangalore", "Karnataka", "580003", "1234567896", "asha@gmail.com", "Book1", "Friends");
+            table.Rows.Add("Porvi", "Shetty", "2nd cross", "Mandya", "Karnataka", "560078", "0987654321", "porvi@gmail.com", "Book2", "Family");
+            table.Rows.Add("Raja", "M", "2nd cross Rajaji Nagar", "Gadag", "Karnataka", "570006", "0987654321", "raja@gmail.com", "Book3", "Profession");
 
             return table;
         }
