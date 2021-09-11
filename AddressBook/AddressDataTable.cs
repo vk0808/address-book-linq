d9f5b92a15317f685f32b5a06010f1dd8a710561
vk0808 <vk4765712@gmail.com>
2021-09-11 13:53:32 +0530

    feat: add rows to dataTable

--- AddressBook/AddressDataTable.cs
+++ AddressBook/AddressDataTable.cs
@@ -22,6 +22,14 @@ namespace AddressBook
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
 
+            // uc-3
+            table.Rows.Add("Rahul ", "Gowda", "2nd cross Ramnagar Nagar", "Mysore", "Karnataka", "580082", "8123351458", "rahul@gmail.com");
+            table.Rows.Add("Ravi ", "Kumar", "3nd cross", "Bangalore", "Karnataka", "800078", "934747358", "ravi@gmail.com");
+            table.Rows.Add("Asha", "Kumari", "4nd cross Indira Nagar", "Bangalore", "Karnataka", "580003", "1234567896", "asha@gmail.com");
+            table.Rows.Add("Porvi", "Shetty", "2nd cross", "Mandya", "Karnataka", "560078", "0987654321", "porvi@gmail.com");
+            table.Rows.Add("Raja", "M", "2nd cross Rajaji Nagar", "Gadag", "Karnataka", "570006", "0987654321", "raja@gmail.com");
+
+
             return table;
         }
     }
