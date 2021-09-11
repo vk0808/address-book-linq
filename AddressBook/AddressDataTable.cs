efef32d2175f57e5e0a19514495d64b08ceb7547
vk0808 <vk4765712@gmail.com>
2021-09-11 13:48:18 +0530

    feat: add column headings to dataTable

--- AddressBook/AddressDataTable.cs
+++ AddressBook/AddressDataTable.cs
@@ -11,6 +11,17 @@ namespace AddressBook
         public DataTable AddressBookDataTable()
         {
             DataTable table = new DataTable();
+
+            // uc-2
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
+
             return table;
         }
     }
